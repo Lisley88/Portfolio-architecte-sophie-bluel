5e9abb20ec557feba3a21f0be00979df1f30f305
Liaosha <alyosasha@gmail.com>
2023-10-07 22:12:03 +0200

    Création des boutons des filtres via l'API et CSS

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,22 +1,36 @@
-//Recuperer les donnes sur API 
-// let gallery =[]
-async function getWorks(){
-   await fetch("http://localhost:5678/api/works")
-   .then( response => response.json())
-   .then( data => {
-        let gallery = document.querySelector(".gallery")
-        data.forEach(works => {
-            gallery.innerHTML +=  ` 
-                <figure class="item">
+//Recuperer les donnes des gallery sur API
+async function getWorks() {
+  const response = await fetch("http://localhost:5678/api/works");
+  const data = await response.json();
+
+  let gallery = document.querySelector(".gallery");
+  data.forEach((works) => {
+    gallery.innerHTML += ` 
+                <figure>
                     <img src="${works.imageUrl}" alt="${works.title}">
                     <figcaption>${works.title}</figcaption>
-                </figure>`
-          })
-      })
- }
+                </figure>`;
+  });
+}
+getWorks();
 
- getWorks()
- 
-   
- 
+// Récupération dynamique des Catégories
+async function getcategory() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const category = await response.json();
 
+  const filtres = document.querySelector(".filter");
+  for (let i = 0; i < category.length; i++) {
+    const btnElement = document.createElement("button");
+    btnElement.innerText = category[i].name;
+    filtres.classList.add("filter");
+    btnElement.classList.add("button");
+    filtres.appendChild(btnElement);
+  }
+  // Créer le bouton tous
+  const btnTous = document.createElement("button");
+  btnTous.innerText = "Tous";
+  btnTous.classList.add("button");
+  filtres.insertBefore(btnTous, filtres.children[0]);
+}
+getcategory();
